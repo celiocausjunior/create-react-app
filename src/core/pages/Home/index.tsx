722f4fd5009e0964064896e0e969948cc2c0356a
Celio Caus Junior <celiocausjunior@gmail.com>
2020-11-15 16:42:03 -0300

    Home Page Completion

--- src/core/pages/Home/index.tsx
+++ src/core/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.scss';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
     <div className="home-container">
@@ -9,7 +10,12 @@ const Home = () => (
                 <p className="text-subtitle">Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior.<br /><br />
                 Favor observar as instruções passadas no capítulo sobre a elaboração deste projeto.<br /> <br />
                 Este design foi adaptado a partir de Ant Design System for Figma, de Mateusz Wierzbicki: <span className="email-subtitle">antforfigma@gmail.com</span>
-             </p>
+                </p>
+            </div>
+            <div className="button-home-page">
+                <Link to="/search">
+                    <button type="button" className="btn btn-primary">Começar</button>
+                </Link>
             </div>
         </div>
     </div>
